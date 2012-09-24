2e144a0c251419cb4667f478fc18b209d5897b30
Andrei Dragomir <adragomir@gmail.com>
2012-09-24 16:34:56 +0300

    added libm2handler

---
+++ libm2handler.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Libm2handler < Formula
+  head 'https://github.com/xrl/libm2handler.git'
+  homepage 'https://github.com/xrl/libm2handler'
+
+  depends_on 'zeromq'
+  depends_on 'jansson'
+
+  def install
+    # Build in serial. See:
+    # https://github.com/mxcl/homebrew/issues/8719
+    ENV.j1
+
+    # Mongrel2 pulls from these ENV vars instead
+    ENV['OPTFLAGS'] = "#{ENV.cflags} #{ENV.cppflags}"
+    ENV['OPTLIBS'] = ENV.ldflags
+
+    system "glibtoolize -c"
+    system "autoreconf -fv --install"
+    system "autoreconf -fv --install"
+    system "./configure", "PREFIX=#{prefix}"
+    system "make", "PREFIX=#{prefix}"
+    system "make install", "PREFIX=#{prefix}"
+  end
+end

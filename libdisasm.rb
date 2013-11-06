366f56842295e0c44f24ffad2a3d815c1952144b
Andrei Dragomir <adragomir@gmail.com>
2013-11-06 13:07:52 +0200

    added libdisasm

---
+++ libdisasm.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Libdisasm < Formula
+  homepage ''
+  url 'http://downloads.sourceforge.net/project/bastard/libdisasm/0.23/libdisasm-0.23.tar.gz'
+  sha1 'ff5e2ffb726d1c28396b21e0abbe10bae5d4a846'
+
+  # depends_on 'cmake' => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.j1  # if your formula's build system can't parallelize
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+end

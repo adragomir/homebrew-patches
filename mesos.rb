6c4202fb72a0d319c1c3167759c2d2238deddb46
Andrei Dragomir <adragomi@adobe.com>
2014-05-27 13:11:31 +0300

    fixed mesos

--- mesos.rb
+++ mesos.rb
@@ -4,7 +4,7 @@ class Mesos < Formula
   homepage 'http://mesos.apache.org'
   url 'http://mirrors.hostingromania.ro/apache.org/mesos/0.18.2/mesos-0.18.2.tar.gz'
   head 'https://github.com/apache/mesos.git'
-  sha1 '0b8e7ebd9c8a28f073b955f7229c5a28ee2d7120'
+  sha1 '7d99075b4ab329171a0f2b12a064232dd23350f8'
 
   def install
     system "./configure", "--disable-debug",

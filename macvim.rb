fd80e8ceb53b25b969a484592d9b3dad6be43361
Andrei Dragomir <adragomi@adobe.com>
2014-10-28 17:41:40 +0200

    macvim

--- macvim.rb
+++ macvim.rb
@@ -1,65 +1,95 @@
 require 'formula'
 
+# Reference: https://github.com/b4winckler/macvim/wiki/building
 class Macvim < Formula
   homepage 'http://code.google.com/p/macvim/'
-  url 'https://github.com/b4winckler/macvim/tarball/snapshot-64'
-  version '7.3-64'
-  sha1 'c8bf2d758f52a1173112138fefbf4e5ab08015ff'
+  url 'https://github.com/b4winckler/macvim/archive/snapshot-73.tar.gz'
+  version '7.4-73'
+  sha1 'b87e37fecb305a99bc268becca39f8854e3ff9f0'
 
   head 'https://github.com/b4winckler/macvim.git', :branch => 'master'
 
   option "custom-icons", "Try to generate custom document icons"
   option "override-system-vim", "Override system vim"
 
-  def options
-  [
-    ["--with-cscope", "Build with Cscope support."],
-    ["--with-lua", "Build with Lua scripting support."]
-  ]
-  end
+  depends_on :xcode => :build
+  depends_on 'cscope' => :recommended
+  depends_on 'lua' => :optional
+  depends_on 'luajit' => :optional
+  depends_on :python => :recommended
+  depends_on :python3 => :optional
 
-  depends_on 'cscope' if ARGV.include? '--with-cscope'
-  depends_on 'lua' if ARGV.include? '--with-lua'
+  env :std if MacOS.version <= :snow_leopard
+  # Help us! We'd like to use superenv in these environments too
 
   def install
+    # MacVim doesn't have and required any Python package, unset PYTHONPATH.
+    ENV.delete('PYTHONPATH')
+
     # Set ARCHFLAGS so the Python app (with C extension) that is
     # used to create the custom icons will not try to compile in
     # PPC support (which isn't needed in Homebrew-supported systems.)
-    arch = MacOS.prefer_64_bit? ? 'x86_64' : 'i386'
-    ENV['ARCHFLAGS'] = "-arch #{arch}"
-    ENV['CC'] = 'clang'
-    ENV['CXX'] = 'clang'
+    ENV['ARCHFLAGS'] = "-arch #{MacOS.preferred_arch}"
+
+    # If building for 10.7 or up, make sure that CC is set to "clang".
+    ENV.clang if MacOS.version >= :lion
+
+    # macvim only works with the current Ruby.framework because it builds with -framework Ruby
+    system_ruby = "/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin/ruby"
 
     args = %W[
       --with-features=huge
-      --with-tlib=ncurses
       --enable-multibyte
-      --with-macarchs=#{arch}
+      --with-macarchs=#{MacOS.preferred_arch}
       --enable-perlinterp
-      --enable-pythoninterp
       --enable-rubyinterp
       --enable-tclinterp
+      --with-tlib=ncurses
+      --with-compiledby=Homebrew
+      --with-local-dir=#{HOMEBREW_PREFIX}
     ]
-      #--with-ruby-command=/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby
+    //--with-ruby-command=#{system_ruby}
 
-    args << "--enable-cscope" if ARGV.include? "--with-cscope"
+    args << "--enable-cscope" if build.with? "cscope"
 
-    if ARGV.include? "--with-lua"
+    if build.with? "lua"
       args << "--enable-luainterp"
       args << "--with-lua-prefix=#{HOMEBREW_PREFIX}"
     end
 
-    system "./configure", *args
-
-    # Building custom icons fails for many users, so off by default.
-    unless build.include? "custom-icons"
-      inreplace "src/MacVim/icons/Makefile", "$(MAKE) -C makeicns", ""
-      inreplace "src/MacVim/icons/make_icons.py", "dont_create = False", "dont_create = True"
+    if build.with? "luajit"
+      args << "--enable-luainterp"
+      args << "--with-lua-prefix=#{HOMEBREW_PREFIX}"
+      args << "--with-luajit"
     end
 
-    # Reference: https://github.com/b4winckler/macvim/wiki/building
-    cd 'src/MacVim/icons' do
-      system "make getenvy"
+    # only allow either python or python3; if the optional
+    # python3 is chosen, default to it, otherwise use python2
+    if build.with? "python3"
+      args << "--enable-python3interp"
+    elsif build.with? "python"
+      ENV.prepend "LDFLAGS", `python-config --ldflags`.chomp
+      ENV.prepend "CFLAGS", `python-config --cflags`.chomp
+      args << "--enable-pythoninterp"
+    end
+
+    # configure appends "SDKS/..." to the value of `xcode-select -print-path`,
+    # but this isn't correct on recent Xcode, so we need to set it manually.
+    # FIXME this is a bug, and it should be fixed upstream.
+    unless MacOS::CLT.installed?
+      args << "--with-developer-dir=#{MacOS::Xcode.prefix}/Platforms/MacOSX.platform/Developer"
+      args << "--with-macsdk=#{MacOS.version}"
+    end
+
+    system "./configure", *args
+
+    if build.include? "custom-icons"
+      # Get the custom font used by the icons
+      system "make", "-C", "src/MacVim/icons", "getenvy"
+    else
+      # Building custom icons fails for many users, so off by default.
+      inreplace "src/MacVim/icons/Makefile", "$(MAKE) -C makeicns", ""
+      inreplace "src/MacVim/icons/make_icons.py", "dont_create = False", "dont_create = True"
     end
 
     system "make"
@@ -70,19 +100,33 @@ class Macvim < Formula
     bin.install "src/MacVim/mvim"
 
     # Create MacVim vimdiff, view, ex equivalents
-    executables = %w[mvimdiff mview mvimex]
+    executables = %w[mvimdiff mview mvimex gvim gvimdiff gview gvimex]
     executables += %w[vi vim vimdiff view vimex] if build.include? "override-system-vim"
-    executables.each {|f| ln_s bin+'mvim', bin+f}
+    executables.each { |e| bin.install_symlink "mvim" => e }
   end
 
-  def caveats; <<-EOS.undent
-    MacVim.app installed to:
-      #{prefix}
+  def caveats
+    if build.with? "python" and build.with? "python3"
+      <<-EOS.undent
+        MacVim can no longer be brewed with dynamic support for both Python versions.
+        Only Python 3 support has been provided.
+      EOS
+    end
+  end
 
-    To link the application to a normal Mac OS X location:
-        brew linkapps
-    or:
-        ln -s #{prefix}/MacVim.app /Applications
-    EOS
+  test do
+    # Simple test to check if MacVim was linked to Python version in $PATH
+    if build.with? "python"
+      vim_path = prefix/"MacVim.app/Contents/MacOS/Vim"
+
+      # Get linked framework using otool
+      otool_output = `otool -L #{vim_path} | grep -m 1 Python`.gsub(/\(.*\)/, "").strip.chomp
+
+      # Expand the link and get the python exec path
+      vim_framework_path = Pathname.new(otool_output).realpath.dirname.to_s.chomp
+      system_framework_path = `python-config --exec-prefix`.chomp
+
+      assert_equal system_framework_path, vim_framework_path
+    end
   end
 end
